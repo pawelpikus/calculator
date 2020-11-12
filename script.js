b1704a14a5d5b99b04540064c41e0bad3e87d56c
Pawel Pikus <pawelpikus7@gmail.com>
2020-11-12 20:15:43 +0100

    egde cases

--- script.js
+++ script.js
@@ -14,8 +14,28 @@ keys.addEventListener('click', e => {
             return Array.from(key.parentNode.children)
                 .forEach(k => k.classList.remove('is-depressed'));
         };
+        // doesn't fit here but where?!
+        const calculate = (v1, op, v2) => {
+            let result = '';
+            switch (op) {
+                case 'add':
+                    result = parseFloat(v1) + parseFloat(v2);
+                    break;
+                case 'substract':
+                    result = parseFloat(v1) - parseFloat(v2);
+                    break;
+                case 'multiply':
+                    result = parseFloat(v1) * parseFloat(v2);
+                    break;
+                case 'divide':
+                    result = parseFloat(v1) / parseFloat(v2);
+                    break;
+            }
+            return result;
+        }
 
         if (!action) {
+            calculator.dataset.previousKeyType = 'number';
             if (displayedNum === '0' || previousKeyType === 'operator') {
                 display.textContent = keyContent;
                 calculator.removeAttribute("data-previous-key-type");
@@ -30,6 +50,15 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
         ) {
+
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            if (firstValue && operator){
+                display.textContent = calculate(firstValue, operator, secondValue);
+            }
+
             removePressed();
             key.classList.add('is-depressed');
             calculator.dataset.previousKeyType = 'operator';
@@ -38,40 +67,30 @@ keys.addEventListener('click', e => {
         }
 
         if (action === 'decimal') {
+            
             if (!displayedNum.includes('.')) {
                 display.textContent = displayedNum + '.';
+            } 
+            
+            if (previousKeyType==='operator') {
+                display.textContent = '0.';
             }
-
+            calculator.dataset.previousKeyType = 'decimal';
         }
         if (action === 'clear') {
             display.textContent = '0';
+            removePressed();
+            calculator.dataset.previousKeyType = 'clear';
         }
         if (action === 'calculate') {
+            calculator.dataset.previousKeyType = 'calculate';
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
             const secondValue = displayedNum;
 
-            const calculate = (v1, op, v2) => {
-                let result = '';
-                switch (op) {
-                    case 'add':
-                        result = parseFloat(v1) + parseFloat(v2);
-                        break;
-                    case 'substract':
-                        result = parseFloat(v1) - parseFloat(v2);
-                        break;
-                    case 'multiply':
-                        result = parseFloat(v1) * parseFloat(v2);
-                        break;
-                    case 'divide':
-                        result = parseFloat(v1) / parseFloat(v2);
-                        break;
-                }
-                return result;
-            }
-
             display.textContent = calculate(firstValue, operator, secondValue);
             removePressed();
+            
 
 
         }
